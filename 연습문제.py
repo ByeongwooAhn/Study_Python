ad379e09c815f5fd299074e7e64fa69bcf2a4786
abyungwoo <abyungwoo@gmail.com>
2022-10-17 19:40:18 +0900

    연습문제 1번으로 과목의 총점과 평균을 구하는 프로그램 입니다.

---
+++ 연습문제.py
@@ -0,0 +1,15 @@
+korean = 80
+english = 75
+math = 55
+total = 0
+aver = 0
+
+total = korean + english + math
+aver = total / 3
+
+print("Q1. 과목의 평균 구하기.")
+print("국어: %d점" % korean)
+print("영어: %d점" % english)
+print("수학: %d점" % math)
+print("총점: %d점" % total)
+print("평균: %d점" % aver)
