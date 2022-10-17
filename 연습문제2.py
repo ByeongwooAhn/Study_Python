497efccc731810708566cf23e55979d598ecc4c9
abyungwoo <abyungwoo@gmail.com>
2022-10-17 19:54:50 +0900

    연습문제 2번으로 숫자의 홀수와 짝수를 판별하는 프로그램  입니다.

---
+++ 연습문제2.py
@@ -0,0 +1,14 @@
+num = 13
+answer = 0
+
+answer = num % 2
+
+print("Q2. 13이 홀수인지 짝수인지 판별하시오.")
+if answer == 1:
+    print("%d은 홀수입니다." % num)
+
+elif answer == 0:
+    print("%d은 짝수입니다." % num)
+
+#파이썬은 if조건문 끝에 :을 붙여야 한다.
+#파이썬은 else if가 아닌 elif를 쓴다.
