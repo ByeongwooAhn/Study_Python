249b984acfde2102927fa8ae61bfc6ca02bfd0f8
abyungwoo <abyungwoo@gmail.com>
2022-10-18 20:04:14 +0900

    연습문제 7번으로 문자열의 join함수를 이용하여 리스트를 문자열로 바꾸어 출력하는 프로그램 입니다.

---
+++ 연습문제7.py
@@ -0,0 +1,8 @@
+a = ['Life', 'is', 'too', 'short']
+
+print("Q7. ['Life', 'is', 'too', 'short']리스트를 문자열의 join 함수를 사용하여 Life is too short 문자열로 바꾸어 출력하시오.")
+print(a)
+
+print(" ".join(a))
+#join은 문자열 사이사이에 원하는 문자열을 추가하는 함수이다.
+#"추가하고자 하는 문자열".join(원래의 문자열)
